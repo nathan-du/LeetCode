09e77dae91e789d370555b97cdbf0c3488611b94
d673650096 <ddyvas@gmail.com>
2016-04-19 14:53:18 +0800

    [111. Minimum Depth of Binary Tree][Accepted]committed by LeetCode Extension

---
+++ 111-Minimum-Depth-of-Binary-Tree/solutionWithoutRecursion.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+import java.util.LinkedList;
+public class Solution {
+    public int minDepth(TreeNode root){  
+        if(root == null) return 0;
+        
+        LinkedList<TreeNode> treeNode = new LinkedList<TreeNode>();
+        LinkedList<Integer> count = new LinkedList<Integer>();
+        treeNode.add(root);
+        count.add(1);
+        
+        while(!treeNode.isEmpty())
+        {
+            TreeNode curr = treeNode.poll();
+            int currCount = count.poll();
+            if(curr.left != null)
+            {
+                treeNode.add(curr.left);
+                count.add(currCount + 1);
+            }
+            if(curr.right != null)
+            {
+                treeNode.add(curr.right);
+                count.add(currCount + 1);
+            }
+            if(curr.left == null && curr.right == null)
+            return currCount;
+        }
+        return 0;
+    }  
+}

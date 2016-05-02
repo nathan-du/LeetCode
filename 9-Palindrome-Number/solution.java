8b12635706f616cca317b8e9dd8d557b15381923
nathan-du <ddyvas@gmail.com>
2016-05-02 13:42:29 +0800

    Update solution.java

--- 9-Palindrome-Number/solution.java
+++ 9-Palindrome-Number/solution.java
@@ -1,13 +1,10 @@
 public class Solution {
     public boolean isPalindrome(int x) {
-       if(x == 0) return true;
-        String temp = String.valueOf(x);
-        if(x < 0)
-        {
-            return false;
-        }
+        if(x == 0) return true;
+        if(x < 0) return false;
         else if(x > 0)
         {
+            String temp = String.valueOf(x);
             for(int i = 0; i <= temp.length()/2 - 1; i++)
             {
                 if(!(temp.charAt(i) == temp.charAt(temp.length()-i-1))) return false;

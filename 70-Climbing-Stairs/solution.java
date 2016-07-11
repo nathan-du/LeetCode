affa768221c64450e8e8118057d721c4f0f3c4c8
nathan-du <ddyvas@gmail.com>
2016-07-11 21:17:58 +0800

    [70. Climbing Stairs][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 70-Climbing-Stairs/solution.java
@@ -0,0 +1,25 @@
+public class Solution {
+    private int times = 0;
+    public int climbStairs(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        recursion(n);
+        return times;
+    }
+    
+    public void recursion(int n)
+    {
+        if(n - 2 >= 0) 
+        {
+            if(n - 2 == 0) times++;
+            recursion(n - 2);
+        }
+        if(n - 1 >= 0) 
+        {
+            if(n - 1 == 0) times++;
+            recursion(n - 1);
+        }
+    }
+    
+    
+}

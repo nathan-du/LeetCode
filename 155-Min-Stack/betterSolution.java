8e9069e162e8974bbe5513d7cd8041f0f2769680
nathan-du <ddyvas@gmail.com>
2016-08-17 14:31:02 -0400

    [155. Min Stack][Accepted]committed by LeetCode Extension

---
+++ 155-Min-Stack/betterSolution.java
@@ -0,0 +1,52 @@
+public class MinStack {
+    private Node head;
+    
+    /** initialize your data structure here. */
+    public MinStack() {
+        
+    }
+    
+    public void push(int x) {
+        if(head == null) head = new Node(x, x);
+        else{
+            head = new Node(x, Math.min(head.min, x), head);
+        }
+    }
+    
+    public void pop() {
+        head = head.next;
+    }
+    
+    public int top() {
+        return head.val;
+    }
+    
+    public int getMin() {
+        return head.min;
+    }
+    
+    private class Node{
+        int val;
+        int min;
+        Node next;
+        
+        public Node(int val, int min){
+            this(val, min, null);
+        }
+        
+        public Node(int val, int min, Node next){
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */

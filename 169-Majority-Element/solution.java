5cadf26ded44d972b17ec24fcea8b13119ef0597
nathan-du <ddyvas@gmail.com>
2016-08-19 10:29:07 -0400

    [169. Majority Element][Accepted]committed by LeetCode Extension

--- 169-Majority-Element/solution.java
+++ 169-Majority-Element/solution.java
@@ -1,13 +1,14 @@
 public class Solution {
     public int majorityElement(int[] nums) {
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        for(int i = 0; i < nums.length; i++) {
+        int length = nums.length;
+        for(int i = 0; i < length; i++) {
             if(!map.containsKey(nums[i])) {
                 map.put(nums[i], 1);
             }
             else {
                 int times = map.get(nums[i]) + 1;
-                if(times > nums.length/2) return nums[i];
+                if(times > length/2) return nums[i];
                 map.put(nums[i], times);
             }
         }

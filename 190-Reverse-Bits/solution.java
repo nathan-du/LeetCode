94e6a6874494153c8d16652701be576206b8f17b
nathan-du <ddyvas@gmail.com>
2016-08-24 13:08:31 -0400

    [190. Reverse Bits][Accepted]committed by LeetCode Extension

---
+++ 190-Reverse-Bits/solution.java
@@ -0,0 +1,14 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int bit = 0;
+        int result = 0;
+        for(int i = 0; i < 32; i++) {
+            bit = n & (1 << i);
+            if(bit != 0) {
+                result += 1 << (31 - i);
+            }
+        }
+        return result;
+    }
+}

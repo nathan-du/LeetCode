8389c63aedfd7047cc3d58c43dcad7bd7b68c257
nathan-du <ddyvas@gmail.com>
2016-08-24 21:23:47 -0400

    [198. House Robber][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 198-House-Robber/solution2.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int rob(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        if(nums.length == 1) return nums[0];
+        if(nums.length == 2) return Math.max(nums[0], nums[1]);
+        return helper(nums, 0, nums.length - 1);
+    }
+    
+    public int helper(int[] nums, int front, int end) {
+        if(front > end) return 0;
+        if(front == end) return nums[end];
+        if(front + 1 == end) return Math.max(nums[front], nums[end]);
+        int temp1 = helper(nums, front + 1, end);
+        int temp2 = helper(nums, front + 2, end);
+        if(temp2 + nums[front] > temp1) return temp2 + nums[front];
+        return temp1;
+    }
+}

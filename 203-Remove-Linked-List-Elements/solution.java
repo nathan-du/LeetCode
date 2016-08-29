da0de419e5f4af27f36984b831ff05c2832f614b
nathan-du <ddyvas@gmail.com>
2016-08-29 11:34:51 -0400

    [203. Remove Linked List Elements][Accepted]committed by LeetCode Extension

--- 203-Remove-Linked-List-Elements/solution.java
+++ 203-Remove-Linked-List-Elements/solution.java
@@ -9,12 +9,17 @@
 public class Solution {
     public ListNode removeElements(ListNode head, int val) {
         if(head == null) return null;
-        ListNode temp = head;
-        while(temp != null) {
-            if(temp.val == val) {
-                temp = temp.next;
+        ListNode temp = new ListNode(0);
+        temp.next = head;
+        ListNode newHead = temp;
+        while(temp != null && temp.next != null) {
+            ListNode p = temp.next;
+            while(p != null && p.val == val) {
+                p = p.next;
             }
+            temp.next = p;
+            temp = temp.next;
         }
-        return temp;
+        return newHead.next;
     }
 }

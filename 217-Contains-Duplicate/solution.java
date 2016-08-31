7631a2bca892827d3071ec37bc59503d5f24eaaf
nathan-du <ddyvas@gmail.com>
2016-08-31 10:52:22 -0400

    [217. Contains Duplicate][Accepted]committed by LeetCode Extension

--- 217-Contains-Duplicate/solution.java
+++ 217-Contains-Duplicate/solution.java
@@ -1,7 +1,7 @@
 public class Solution {
     public boolean containsDuplicate(int[] nums) {
         if(nums.length <= 1) return false;
-        HashSet<Integer> set = new HashSet<Integer>();
+        Set<Integer> set = new HashSet<Integer>();
         for(int i = 0; i < nums.length; i++) {
             if(set.contains(nums[i])) return true;
             else {

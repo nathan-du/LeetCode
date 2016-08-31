3aabeebae6491c18280bea18f64e68947a47d6db
nathan-du <ddyvas@gmail.com>
2016-08-31 10:00:40 -0400

    [206. Reverse Linked List][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 206-Reverse-Linked-List/solution1.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode first = head;
+        ListNode second = head.next;
+        while(second != null) {
+            ListNode temp = second.next;
+            second.next = first;
+            first = second;
+            second = temp;
+        }
+        return first;
+    }
+}

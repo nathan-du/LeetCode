1730ea601d985689341be35b3688aa2f41ed606f
nathan-du <ddyvas@gmail.com>
2016-08-31 10:48:48 -0400

    [217. Contains Duplicate][Time Limit Exceeded]committed by LeetCode Extension

--- 217-Contains-Duplicate/solution.java
+++ 217-Contains-Duplicate/solution.java
@@ -1,5 +1,6 @@
 public class Solution {
     public boolean containsDuplicate(int[] nums) {
+        if(nums.length <= 1) return false;
         HashSet<Integer> set = new HashSet<Integer>();
         for(int i = 0; i < nums.length; i++) {
             if(set.contains(nums[i])) return true;

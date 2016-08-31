c2936f08a7bb279a67c771b2f5f53a1e1baba1da
nathan-du <ddyvas@gmail.com>
2016-08-31 15:14:26 -0400

    [220. Contains Duplicate III][Time Limit Exceeded]committed by LeetCode Extension

--- 220-Contains-Duplicate-III/solution1.java
+++ 220-Contains-Duplicate-III/solution1.java
@@ -1,6 +1,6 @@
 public class Solution {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        if(k < 1 || t < 0) return false;
+        if(nums.length <= 1 || k < 1 || t < 0) return false;
         for(int c = 1; c <= k; c++) {
             for(int i = 0; i < nums.length -c; i++) {
                 int j = i + c;

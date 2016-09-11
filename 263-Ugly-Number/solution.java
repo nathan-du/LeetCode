c1d677dc66b3892e8ba15ddadfd420fb8c177cba
nathan-du <ddyvas@gmail.com>
2016-09-10 21:04:35 -0400

    [263. Ugly Number][Accepted]committed by LeetCode Extension

--- 263-Ugly-Number/solution.java
+++ 263-Ugly-Number/solution.java
@@ -1,12 +1,12 @@
 public class Solution {
     public boolean isUgly(int num) {
-        while(num % 2 == 0) {
+        while(num % 2 == 0 && num > 0) {
             num /= 2;
         }
-        while(num % 3 == 0) {
+        while(num % 3 == 0 && num > 0) {
             num /= 3;
         }
-        while(num % 5 == 0) {
+        while(num % 5 == 0 && num > 0) {
             num /= 5;
         }
         return num == 1;

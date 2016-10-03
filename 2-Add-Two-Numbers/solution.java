ff1d4215dcce08287f504666a775479306c4ea49
nathan-du <ddyvas@gmail.com>
2016-10-03 12:47:21 -0400

    [2. Add Two Numbers][Accepted]committed by LeetCode Extension

---
+++ 2-Add-Two-Numbers/solution.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode prev = new ListNode(0);
+        ListNode head = prev;
+        int carry = 0;
+        while(l1 !=null || l2 !=null || carry != 0) {
+            ListNode cur = new ListNode(0);
+            int sum = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + carry;
+            cur.val = sum % 10;
+            carry = sum / 10;
+            prev.next = cur;
+            prev = cur;
+            
+            l1 = (l1 == null ? l1 : l1.next);
+            l2 = (l2 == null ? l2 : l2.next);
+        }
+        return head.next;
+
+    }
+}
